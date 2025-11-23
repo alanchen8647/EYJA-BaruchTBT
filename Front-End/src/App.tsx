c5ce815109252b6219d24d4fc1f37ddca166679f
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-11-23 15:39:23 -0500

    Create the first page of the project and add a navbar, hero image, and a card to it.

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Accordion from 'react-bootstrap/Accordion';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import { useState } from "react";
+import "./App.css";
+import Accordion from "react-bootstrap/Accordion";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Textbooks from "../images/textbooks.jpg";
+import Textbooks1 from "../images/textbooks1.jpg";
+import Mathtextbook from "../images/math-textbook.jpg";
 
 function App() {
-
   fetch("/api/test-get")
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
   return (
     <>
-      <Accordion defaultActiveKey="0">
+      {/* <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
         <Accordion.Header>Accordion Item #1</Accordion.Header>
         <Accordion.Body>
@@ -38,9 +38,102 @@ function App() {
           culpa qui officia deserunt mollit anim id est laborum.
         </Accordion.Body>
       </Accordion.Item>
-    </Accordion>
+    </Accordion> */}
+      <header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Cart
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Sell
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Dicussion/Community
+                  </a>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <div className="container my-5">
+        <img
+          src={Textbooks1}
+          className="img-fluid"
+          alt="Pile of textbooks"
+          style={{ height: "500px" }}
+        ></img>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-1">
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                src={Mathtextbook}
+                className="card-img-top"
+                alt="A calculus textbook"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card’s content.
+                </p>
+                <div>
+                  <a href="#" className="btn btn-primary mb-3">
+                    More infomation
+                  </a>
+                </div>
+                <div>
+                  <a href="#" className="btn btn-primary">
+                    Add to cart
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
