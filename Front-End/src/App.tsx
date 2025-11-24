cdc921b702bc704d0db898376c7b8661a6b41e02
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-11-23 23:05:33 -0500

    Create a sell page to allow the user to add any books they want to sell to the website.

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { useState } from "react";
 import "./App.css";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Textbooks from "../images/textbooks.jpg";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import SellPage from "./SellPage.tsx";
 import Textbooks1 from "../images/textbooks1.jpg";
 import Mathtextbook from "../images/math-textbook.jpg";
 
@@ -11,39 +13,14 @@ function App() {
     .then((data) => console.log(data));
 
   return (
-    <>
-      {/* <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion> */}
+    <BrowserRouter>
       <header>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <Link className="navbar-brand" to="/">
               Navbar
-            </a>
+            </Link>
+
             <button
               className="navbar-toggler"
               type="button"
@@ -55,27 +32,30 @@ function App() {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
                     Cart
                   </a>
                 </li>
+
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
+                  <Link className="nav-link active" to="/sell">
                     Sell
-                  </a>
+                  </Link>
                 </li>
+
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Dicussion/Community
+                  <a className="nav-link active" href="#">
+                    Discussion/Community
                   </a>
                 </li>
+
               </ul>
+
               <form className="d-flex" role="search">
                 <input
                   className="form-control me-2"
@@ -87,51 +67,59 @@ function App() {
                   Search
                 </button>
               </form>
+
             </div>
           </div>
         </nav>
       </header>
 
-      <div className="container my-5">
-        <img
-          src={Textbooks1}
-          className="img-fluid"
-          alt="Pile of textbooks"
-          style={{ height: "500px" }}
-        ></img>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-1">
-            <div className="card" style={{ width: "18rem" }}>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="container my-5">
               <img
-                src={Mathtextbook}
-                className="card-img-top"
-                alt="A calculus textbook"
+                src={Textbooks1}
+                className="img-fluid"
+                alt="Pile of textbooks"
+                style={{ height: "500px" }}
               />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-                <div>
-                  <a href="#" className="btn btn-primary mb-3">
-                    More infomation
-                  </a>
-                </div>
-                <div>
-                  <a href="#" className="btn btn-primary">
-                    Add to cart
-                  </a>
+            </div>
+
+            <div className="container">
+              <div className="row">
+                <div className="col-1">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img
+                      src={Mathtextbook}
+                      className="card-img-top"
+                      alt="A calculus textbook"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text.
+                      </p>
+                      <div>
+                        <a href="#" className="btn btn-primary mb-3">
+                          More information
+                        </a>
+                      </div>
+                      <div>
+                        <a href="#" className="btn btn-primary">
+                          Add to cart
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
+          </>
+        } />
+
+        <Route path="/sell" element={<SellPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Front-End/src/SellPage.tsx
@@ -0,0 +1,95 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function SellPage() {
+  fetch("/api/test-get")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+  return (
+    <>
+      <form>
+        <div className="container my-5">
+          <div className="mb-4">
+            <label htmlFor="formGroupExampleInput" className="form-label" style={{fontWeight: "bold"}}>
+              Textbook Title
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput"
+              placeholder="E.g. Calculus by Gilbert Strang"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="formGroupExampleInput2" className="form-label" style={{fontWeight: "bold"}}>
+              Subject
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput2"
+              placeholder="E.g. Math"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="formGroupExampleInput" className="form-label" style={{fontWeight: "bold"}}>
+              Course Number
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput"
+              placeholder="E.g. MTH 3150"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="formGroupExampleInput2" className="form-label" style={{fontWeight: "bold"}}>
+              Condition
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput2"
+              placeholder="E.g. Used, new, worn-out"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="formGroupExampleInput" className="form-label" style={{fontWeight: "bold"}}>
+              Price
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput"
+              placeholder="E.g. $50.00"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="formGroupExampleInput2" className="form-label" style={{fontWeight: "bold"}}>
+              Seller Contact
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="formGroupExampleInput2"
+              placeholder="E.g. Phone number or Email"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mt-4">
+            Add Textbook
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default SellPage;
