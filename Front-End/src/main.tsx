3e213cfbd260ad1363897fb58a22f8223ec52155
YimingWei <simon040129@gmail.com>
2025-12-11 15:51:37 -0500

    feat: Refine navbar padding, height, and font sizes, and reorder Bootstrap imports.

--- Front-End/src/main.tsx
+++ Front-End/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.jsx';
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
